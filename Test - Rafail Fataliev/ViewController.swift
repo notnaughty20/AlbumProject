8cabde0254b027956f17d8429555a0b1139774ad
notnaughty20 <rafailfataliev01@gmail.com>
2022-06-08 14:17:22 +0300

    Initial Commit

---
+++ Test - Rafail Fataliev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test - Rafail Fataliev
+//
+//  Created by Рафаил Фаталиев on 08.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
